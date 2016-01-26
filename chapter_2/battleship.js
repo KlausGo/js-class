a36a6ca027f054126c62d15ddf82e154465b382b
KlausGo <fender2000@icloud.com>
2016-01-26 19:13:48 +0900

    ..

--- chapter_2/battleship.js
+++ chapter_2/battleship.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 
 var guess;
 var hits = 0;
@@ -8,23 +9,21 @@ var guesses = 0;
 
 var isSunk = false;
 
-
 while (isSunk == false) {
   guess = prompt("준비, 조준, 발사! (0에서 6까지 중 추측한 숫자를 입력하세요):");
+
   if (guess < 0 || guess > 6) {
     alert ("올바른 방 번호를 입력하세요");
   } else {
     guesses = guesses + 1;
     if (guess == location1 || guess == location2 || guess == location3) {
-
       alert("명중!");
       hits = hits + 1 ;
-
-      if (hits == 3) {
-        isSunk = true;
-        alert("전함 침몰!");
-      }
-    } else {
+        if (hits == 3) {
+          isSunk = true;
+          alert("전함 침몰!");
+        }
+      } else {
       alert("실패");
     }
   }
